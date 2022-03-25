b38cd86738da723c6574bab666af4fb46d91409d
r0b-ert <94683494+r0b-ert@users.noreply.github.com>
2022-03-25 01:48:48 +0000

    Changes

--- core/src/main/java/com/mygdx/pirategame/screen/GameScreen.java
+++ core/src/main/java/com/mygdx/pirategame/screen/GameScreen.java
@@ -3,15 +3,18 @@ package com.mygdx.pirategame.screen;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.maps.MapProperties;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.scenes.scene2d.Actor;
@@ -396,6 +399,20 @@ public class GameScreen implements Screen {
                 pause();
             }
         }
+
+        Body body = getPlayer().b2body;
+        Vector2 position = body.getPosition();
+        for (Map.Entry<CollegeMetadata, College> college : colleges.entrySet()) {
+            if (college.getValue().getMetaData().isPlayer()) {
+                float distance = position.dst(college.getValue().getMetaData().getCentrePosition());
+                if (distance < college.getValue().getMetaData().getDistance()) {
+                    System.out.println(distance);
+                    if (Gdx.input.isKeyJustPressed(Input.Keys.E)) {
+                        System.out.println("close");
+                    }
+                }
+            }
+        }
     }
 
     /**
@@ -510,6 +527,18 @@ public class GameScreen implements Screen {
         for (Map.Entry<CollegeMetadata, College> college : colleges.entrySet()) {
             college.getValue().draw(game.batch);
         }
+        /*
+        // Set image
+        Texture point = new Texture("entity/cannonball.png");
+        Sprite circle = new Sprite(point);
+
+        circle.setPosition(6200 / PirateGame.PPM, 1100 / PirateGame.PPM);
+        // scales the sprite depending on window size divided by a constant
+        circle.setSize(100 / PirateGame.PPM, 100 / PirateGame.PPM);
+
+        circle.draw(game.batch);
+
+         */
 
         //Updates all ships
         for (int i = 0; i < ships.size(); i++) {
